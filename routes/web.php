c9f744dd22cf8a075c8a96694f8f13400083fdc3
luuhoang2k4 <135411218+luuhoang-star@users.noreply.github.com>
2026-06-15 08:49:23 +0700

    feat: định nghĩa Routes

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\Frontend\KycController;
+use App\Http\Controllers\Frontend\UserDashboardController;
+use App\Http\Controllers\Frontend\ProfileController;
+use App\Http\Controllers\Frontend\StoreController;
+use App\Http\Controllers\Frontend\VendorDashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('frontend.home.index');
+});
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
+
+/** Profile Routes */
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::put('/profile', [ProfileController::class, 'profileUpdate'])->name('profile.update');
+Route::put('/profile/password', [ProfileController::class, 'passwordUpdate'])->name('password.update');
+
+Route::get('/kyc-verification', [KycController::class, 'index'])->name('kyc.index');
+Route::post('/kyc-verification', [KycController::class, 'store'])->name('kyc.store');
+
+
+
+// Vendor route
+Route::group([
+    'prefix' => 'vendor',
+    'as' => 'vendor.',
+    'middleware' => ['auth', 'verified']
+], function () {
+
+    Route::get('/dashboard', [VendorDashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::resource('store-profile', StoreController::class);
+});
+
+
+require __DIR__ . '/auth.php';
