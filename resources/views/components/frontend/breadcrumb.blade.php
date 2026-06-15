511bda59ed45b32dcd9e9c3dc7cdd787f6de7bb4
luuhoang2k4 <135411218+luuhoang-star@users.noreply.github.com>
2026-06-15 08:52:35 +0700

    feat: thêm View Components và Resources

---
+++ resources/views/components/frontend/breadcrumb.blade.php
@@ -0,0 +1,14 @@
+  <div class="page-header breadcrumb-wrap">
+      <div class="container">
+          <div class="breadcrumb">
+              @foreach ($items as $item)
+                @if(!$loop->last)
+                  <a href="{{ $item['url'] }}" rel="nofollow"><i class="fi-rs-home mr-5"></i>{{ $item['label'] }}</a>
+                @else
+                  <span> {{ $item['label'] }} ></span>
+                @endif
+              @endforeach
+
+          </div>
+      </div>
+  </div>
