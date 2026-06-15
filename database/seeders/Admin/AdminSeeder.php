79cfc9afb388aa3ce8db47b5b353eb69db0006ae
luuhoang2k4 <135411218+luuhoang-star@users.noreply.github.com>
2026-06-15 08:47:27 +0700

    feat: tạo migrations, seeders và factories

---
+++ database/seeders/Admin/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Database\Seeders\Admin;
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = new Admin();
+        $admin->name = 'SUPER ADMIN';
+        $admin->email = 'admin@gmail.com';
+        $admin->password = bcrypt('1234');
+        $admin->save();
+    }
+}
